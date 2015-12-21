7f8adb6a2518da382d313e93ad5beb7a142a887d
Ethan Weeks <ethanweeks@ymail.com>
2015-12-20 17:10:41 -0700

    initial match create page

--- routes/matches.js
+++ routes/matches.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var express = require('express'),
-    router = express.Router();
+     router = express.Router();
 
 var Table = require('../data/knexSetup.js'),
-    Matches = Table('matches');
-
+  Matches = Table('matches'),
+    Users = Table('users');
 // GET ‘/’ - shows all resources TODO
 router.get('/', function(req, res) {
   Matches().select('*')
@@ -19,22 +19,52 @@ router.get('/', function(req, res) {
 
 // GET ‘/new’ - shows new create new resource page TODO
 router.get('/new', function(req, res) {
-  res.send('Takes you to Create new match page');
+  res.render('createMatch', {
+    title : "Create Match"
+  });
 });
 
 // POST ‘/new’ - creates individual TODO
 // curl -d "user1=4&score1=21&user2=3&score2=11" http://localhost:3000/matches/new
 router.post('/new', function(req, res) { // curl -d <queryString> http://localhost:3000/matches/new
   var match = {};
+  var counter = 0;
 
-  match.user_id1 = req.body.user1;
-  match.score1 = req.body.score1;
-  match.user_id2 = req.body.user2;
-  match.score2 = req.body.score2;
+  Users().where({
+    username: req.body.username1
+  })
+  .select('*').then(function(user1Data){
+    console.log(user1Data);
+    match.user_id1 = user1Data[0].id;
+      match.score1 = -1;
+      
+    counter++;
 
-  Matches().insert(match).then(function(){
-    res.send(JSON.stringify(match) + '\n');
+  if(counter > 1) {
+    Matches().insert(match).then(function(){
+      res.send(JSON.stringify(match) + '\n');
+    });
+  }
+
   });
+
+  Users().where({
+    username: req.body.username2
+  })
+  .select('*').then(function(user2Data){
+    match.user_id2 = user2Data[0].id;
+      match.score2 = -1;
+
+    counter++;
+
+    if(counter > 1) {
+      Matches().insert(match).then(function(){
+        res.send(JSON.stringify(match) + '\n');
+      });
+    }
+  });
+
+  
 });
 
 // GET ‘/:id’ - shows individual resource TODO
@@ -77,8 +107,5 @@ router.delete('/:id', function(req, res) { // curl -X DELETE http://localhost:30
   });
 });
 
-router.get('/create', function(req, res){
-  res.render('../createMatch.ejs');
-});
 
 module.exports = router;
