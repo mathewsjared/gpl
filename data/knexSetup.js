2009599c3644595d3d31f5df525a21b0a45db650
Ethan Weeks <ethanweeks@ymail.com>
2015-12-21 10:47:03 -0700

    Moved db configuration to .env

--- data/knexSetup.js
+++ data/knexSetup.js
@@ -1,13 +1,15 @@
 'use strict';
 
+require('dotenv').load();
+
 module.exports = function(){
   var knex = require('knex')({
     client: 'pg',
     connection: {
-      host     : '127.0.0.1',
-      user     : 'Mathews',
-      password : 'password',
-      database : 'gpldb'
+      host     : process.env.DATABASE_URL,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME
     },
     searchPath: 'knex,public',
     debug: true
